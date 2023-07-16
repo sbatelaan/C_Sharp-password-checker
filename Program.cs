0d810cf7cff834fb10141a2825164f4169fd80cb
sbatelaan <119352363+sbatelaan@users.noreply.github.com>
2023-07-15 22:03:52 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+using System;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int minLength = 8;
+            string uppercase = "QWERTYUIOPLKJHGFDSAZXCVBNM";
+            string lowercase = "qwertyuioplkjhgfdsazxcvbnm";
+            string digits = "1234567890";
+            string specialChars = "!@#$%^&*()";
+            
+            Console.WriteLine("Enter a password: ");
+            string userPassword = Console.ReadLine();
+
+            int score = 0;
+
+            if (userPassword.Length >= minLength) {
+                score ++;
+            } if (Tools.Contains(userPassword, uppercase)) {
+                score +=1;
+            } if (Tools.Contains(userPassword, lowercase)) {
+                score ++;
+            } if (Tools.Contains(userPassword, digits)) {
+                score ++;
+            } if (Tools.Contains(userPassword, specialChars)) {
+                score ++;
+            }
+
+            switch (score) {
+                case 1: 
+                Console.WriteLine("weak");
+                break;
+                case 2: 
+                Console.WriteLine("mid");
+                break;
+                case 3: 
+                Console.WriteLine("strong");
+                break;
+                case 4: 
+                Console.WriteLine("very strong");
+                break;
+                case 5: 
+                Console.WriteLine("very strong");
+                break;
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
